15675df01ef531d1075caf60c5fde38ef8aca925
linari facundo <facundo.linari@hotmail.com>
2020-06-12 19:15:15 -0300

    dns.py imprime parecido a dig

---
+++ taller4/src/dns.py
@@ -0,0 +1,21 @@
+import sys
+from scapy.all import *
+
+from functions import print_section
+
+
+if len(sys.argv) <= 1:
+    print("Modo de uso: sudo python3 dns.py <dirección destino>")
+    exit()
+
+qname = sys.argv[1]
+dns = DNS(rd=1,qd=DNSQR(qname=qname))
+udp = UDP(sport=RandShort(), dport=53)
+ip = IP(dst="199.9.14.201") # b.root-servers.net ip
+
+answer = sr1( ip / udp / dns , verbose=0, timeout=10)
+if answer and answer.haslayer(DNS) and answer[DNS].qd.qtype == 1:
+    print_section("ANSWER", answer[DNS].an)
+    print_section("AUTHORITY", answer[DNS].ns)
+    print_section("ADDITIONAL", answer[DNS].ar)
+    
---
+++ taller4/src/functions.py
@@ -0,0 +1,33 @@
+classes = {
+    1:"IN"
+}
+
+types = {
+    1:"A",
+    2:"NS",
+    5:"CNAME",
+    28:"AAAA"
+}
+
+def translate_class(resource_record_class:int) -> str:
+    """Get human readable string from class number (which comes from a ShortEnumField)"""
+    return classes[resource_record_class]
+
+def translate_type(resource_record_type:int) -> str:
+    """Get human readable string from type number"""
+    return types[resource_record_type]
+
+def print_dns_register(resource_record:scapy.layers.dns.DNSRR):
+    domain_name = str(resource_record.rrname)
+    time_to_live = resource_record.ttl
+    register_class = translate_class(resource_record.rclass)
+    register_type = translate_type(resource_record.type)
+    value = str(resource_record.rdata)
+    print("{:30s} {:10d} {:5s} {:5s} {:30s}".format(
+        domain_name, time_to_live, register_class, register_type, value))
+
+def print_section(name:str ,section):
+    if section:
+        print(name + " SECTION:")
+        for reg in section.iterpayloads():
+            print_dns_register(reg)
